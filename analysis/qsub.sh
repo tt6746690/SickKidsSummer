6799770c74c920504b1884207287db4d431541b8
Mark <peiqi1122@gmail.com>
2017-05-23 12:58:23 -0400

    run again on hpf

--- analysis/qsub.sh
+++ analysis/qsub.sh
@@ -1,4 +1,4 @@
-# qsub -W umask=0002 -W group_list=brudno -l vmem=20g,mem=20g
+# qsub -W umask=0002 -W group_list=brudno -l vmem=20g,mem=20g -l walltime=48:00:0
 export PYTHONPATH=/hpf/projects/brudno/wangpeiq/sickkids_summer/analysis/:PYTHONPATH
 module load python/3.5.2 
 python /hpf/projects/brudno/wangpeiq/sickkids_summer/analysis/exon_expr.py
